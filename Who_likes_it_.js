edb8537c77cdc4c802e9d568ade8bf4c255e560c
Gia Bảo <hoanggiabao131012vn@gmail.com>
2026-05-31 19:15:54 +0700

    Create Who_likes_it_.js

---
+++ Who_likes_it_.js
@@ -0,0 +1,12 @@
+function likes(names) {
+  const numberOfNames = names.length;
+  if(numberOfNames === 0) {
+    return "no one likes this";
+  } else if(numberOfNames < 4) {
+    if(numberOfNames === 1) {
+      return `${names[0]} likes this`;
+    } else if(numberOfNames === 2) {
+      return `${names[0]} and ${names[1]} like this`;
+    } else {return `${names[0]}, ${names[1]} and ${names[2]} like this`;}
+  } return `${names[0]}, ${names[1]} and ${numberOfNames - 2} others like this`;
+}
